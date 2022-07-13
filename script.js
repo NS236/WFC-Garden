d9de0112e84c78ab3371b30d7cfaef2978e98433
NS236 <105520205+NS236@users.noreply.github.com>
2022-07-12 20:42:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var SIZE = 15;
-var width = window.innerWidth;
-var height = window.innerHeight;
+const WIDTH = 15;
+const HEIGHT = 15;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-var imgs = [];
-for (var i = 1; i <= 81; i++) {
+let imgs = [];
+for (let i = 1; i <= 81; i++) {
   let img = new Image(100, 100);
   img.src = "images/tile_" + i + ".svg";
   imgs.push(img);
@@ -15,12 +16,13 @@ for (var i = 1; i <= 81; i++) {
 2 - light green
 */
 
-var nullTile = {
+const nullTile = {
+  filled: false,
   rot: null,
-  sides: [-1, -1, -1, -1]
+  sides: [-1, -1, -1, -1],
 };
 
-var tiles = [
+const tiles = [
   {
     rot: 0,
     sides: [0, 0, 0, 0],
@@ -1321,11 +1323,11 @@ var tiles = [
 
 //credit: https://stackoverflow.com/a/966938
 function createArray(length) {
-  var arr = new Array(length || 0),
+  let arr = new Array(length || 0),
     i = length;
 
   if (arguments.length > 1) {
-    var args = Array.prototype.slice.call(arguments, 1);
+    let args = Array.prototype.slice.call(arguments, 1);
     while (i--) {
       arr[length - 1 - i] = createArray.apply(this, args);
     }
@@ -1334,17 +1336,17 @@ function createArray(length) {
   return arr;
 }
 
-var grid = createArray(SIZE, SIZE);
+let grid = createArray(WIDTH, HEIGHT);
 grid.forEach(function (row) {
   row.fill(nullTile);
 });
 // console.log(grid);
 
 function getSides(i, j) {
-  var N = j > 0 ? grid[i][j - 1].sides[2] : -1;
-  var E = i < SIZE - 1 ? grid[i + 1][j].sides[3] : -1;
-  var S = j < SIZE - 1 ? grid[i][j + 1].sides[0] : -1;
-  var W = i > 0 ? grid[i - 1][j].sides[1] : -1;
+  let N = j > 0 ? grid[i][j - 1].sides[2] : -1;
+  let E = i < WIDTH - 1 ? grid[i + 1][j].sides[3] : -1;
+  let S = j < HEIGHT - 1 ? grid[i][j + 1].sides[0] : -1;
+  let W = i > 0 ? grid[i - 1][j].sides[1] : -1;
 
   return [N, E, S, W];
 }
@@ -1353,48 +1355,134 @@ function randArr(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-for (var j = 0; j < SIZE; j++) {
-  for (var i = 0; i < SIZE; i++) {
-    var options = [];
-    var sides = getSides(i, j);
-    // console.log(sides);
-    for (var tile of tiles) {
-      var plausible = true;
-      for (var side = 0; side < sides.length; side++) {
-        if (sides[side] !== -1 && sides[side] !== tile.sides[side]) {
-          plausible = !true;
+function compatible(t, s) {
+  for (let side = 0; side < s.length; side++) {
+    if (s[side] !== -1 && s[side] !== t.sides[side]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function bestSpots() {
+  let spots = [];
+  let bestCount = tiles.length + 1;
+  for (let x = 0; x < WIDTH; x++) {
+    for (let y = 0; y < HEIGHT; y++) {
+      let spot = grid[x][y];
+      let count = 0;
+      if (!spot.filled) {
+        let sides = getSides(x, y);
+        for (let tile of tiles) {
+          if(compatible(tile, sides)) {
+            count++;
+          }
+        }
+
+        if(count == bestCount) {
+          spots.push({x: x, y: y});
+        } else if(count < bestCount) {
+          spots = [{x: x, y: y}];
+          bestCount = count;
         }
       }
-
-      if (plausible) {
-        options.push(tile);
-      }
     }
-
-    var choice = randArr(options);
-    // println("first tile: " + grid[0][0].id);
-    // println("setting position: " + i + ", " + j + " to " + choice.id);
-    grid[i][j] = choice;
-    // println("first tile: " + grid[0][0].id);
-    // println("recent tile: " + grid[i][j].id);
-    // println("\n");
   }
+
+  return spots;
 }
 
 function showTile(tile, i, j) {
-  var scl = (width - 17) / SIZE;
-  var tileImg = imgs[tiles.indexOf(tile) % imgs.length];
-  var img = tileImg.cloneNode(true);
-  img.style.width = scl + "px";
-  img.style.height = scl + "px";
-  img.className = "rot" + tile.rot;
-  document.body.append(img);
+  let scl = (width - 17) / WIDTH;
+  if(tile.filled) {
+    let tileImg = imgs[tiles.indexOf(tile) % imgs.length];
+    let img = tileImg.cloneNode(true);
+    img.style.width = `${scl}px`;
+    img.style.height = `${scl}px`;
+    img.className = "rot" + tile.rot;
+    document.body.append(img);
+  } else {
+    let img = imgs[0].cloneNode(true);
+    img.style.width = `${scl}px`;
+    img.style.height = `${scl}px`;
+    document.body.append(img)
+  }
 }
 
-for (var j = 0; j < SIZE; j++) {
-  for (var i = 0; i < SIZE; i++) {
-    showTile(grid[i][j], i, j);
+let placed = 0;
+
+function run() {
+  let best = bestSpots();
+  let choiceSpot = randArr(best);
+  console.log(choiceSpot);
+
+  let options = [];
+  let sides = getSides(choiceSpot.x, choiceSpot.y);
+  // console.log(sides);
+  for (let tile of tiles) {
+    let plausible = compatible(tile, sides);
+
+    if (plausible) {
+      options.push(tile);
+    }
   }
-  var br = document.createElement("br");
-  document.body.append(br);
-}
+
+  let choice = randArr(options);
+  grid[choiceSpot.x][choiceSpot.y] = choice;
+  grid[choiceSpot.x][choiceSpot.y].filled = true;
+
+  document.body.innerHTML = "";
+  for (let j = 0; j < HEIGHT; j++) {
+    for (let i = 0; i < WIDTH; i++) {
+      showTile(grid[i][j], i, j);
+    }
+    let br = document.createElement("br");
+    document.body.append(br);
+  }
+
+  placed++;
+  if(placed < WIDTH * HEIGHT) {
+    window.requestAnimationFrame(run);
+  }
+};
+
+window.requestAnimationFrame(run);
+
+// for (let i = 0; i < WIDTH * HEIGHT; i++) {
+//   let best = bestSpots();
+//   let choiceSpot = randArr(best);
+//   console.log(choiceSpot);
+
+//   let options = [];
+//   let sides = getSides(choiceSpot.x, choiceSpot.y);
+//   // console.log(sides);
+//   for (let tile of tiles) {
+//     let plausible = compatible(tile, sides);
+
+//     if (plausible) {
+//       options.push(tile);
+//     }
+//   }
+
+//   let choice = randArr(options);
+//   grid[choiceSpot.x][choiceSpot.y] = choice;
+//   grid[choiceSpot.x][choiceSpot.y].filled = true;
+// }
+
+// function showTile(tile, i, j) {
+//   let scl = (width - 17) / WIDTH;
+//   let tileImg = imgs[tiles.indexOf(tile) % imgs.length];
+//   let img = tileImg.cloneNode(true);
+//   img.style.width = scl + "px";
+//   img.style.height = scl + "px";
+//   img.className = "rot" + tile.rot;
+//   document.body.append(img);
+// }
+
+// for (let j = 0; j < HEIGHT; j++) {
+//   for (let i = 0; i < WIDTH; i++) {
+//     showTile(grid[i][j], i, j);
+//   }
+//   let br = document.createElement("br");
+//   document.body.append(br);
+// }
